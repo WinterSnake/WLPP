1caca44386c5e43f9b7a2b46093a095cdb646599
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-06-01 22:48:46 -0400

    add(test): event loop and exit

--- tests/window.cpp
+++ tests/window.cpp
@@ -5,6 +5,7 @@
 
 // Include: Standard Library
 #include <memory>
+#include <string>
 
 // Include: WL
 #include "WL/wl.hpp"
@@ -20,6 +21,14 @@ int main(int argc, char** argv)
 	};
 
 	std::unique_ptr<WL::Window> window = WL::Window::Create(properties);
+
+	while (WL::Event event = window->Poll_Event())
+	{
+		if (event.type == WL::EVENT::KEY_PRESS && event.key == WL::KEY::ESC)
+		{
+			window->Exit_Event_Poll();
+		}
+	}
 	return 0;
 }
 
